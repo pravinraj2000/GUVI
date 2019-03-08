6d41d3a372ac65099601039748b47ba2a929e4c7
pravinraj2000 <47383736+pravinraj2000@users.noreply.github.com>
2019-03-08 13:39:13 +0530

    Create b4 alphabet.java

---
+++ b4 alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  class b4 alphabet {
+
+    public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+      
+ 	char c = input.next().charAt(0); 
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println("Alphabet.");
+        else
+            System.out.println("No");
+    }
+}
